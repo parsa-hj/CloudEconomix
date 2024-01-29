390339402ccd9fe62e0249a3cf9ea14064e22059
Parsa Hajiha <hajihaparsa2004@gmail.com>
2024-01-28 20:14:39 -0600

    surveyjs added

--- src/pages/SurveyPage.js
+++ src/pages/SurveyPage.js
@@ -1,9 +1,99 @@
 import React from 'react'
+import '../assets/styles/survey.css'
+
+import { useCallback, useState, useRef } from 'react';
+
+import 'survey-core/defaultV2.min.css';
+import { LayeredDark } from "survey-core/themes/layered-dark";
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+
+const surveyJson = {
+  pages: [{
+    elements: [{
+      type: "html",
+      html: "<h4>Fill out the information on the form below to find out the best cloud solutions for your business.</h4>"
+    }]
+  }, {
+    elements: [{
+      name: "satisfaction-score",
+      title: "How would you describe your experience with our product?",
+      type: "radiogroup",
+      choices: [
+        { value: 5, text: "Fully satisfying" },
+        { value: 4, text: "Generally satisfying" },
+        { value: 3, text: "Neutral" },
+        { value: 2, text: "Rather unsatisfying" },
+        { value: 1, text: "Not satisfying at all" }
+      ],
+      isRequired: true
+    }]
+  }, {
+    elements: [{
+      name: "what-would-make-you-more-satisfied",
+      title: "What can we do to make your experience more satisfying?",
+      type: "comment",
+      visibleIf: "{satisfaction-score} = 4"
+    }, {
+      name: "nps-score",
+      title: "On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?",
+      type: "rating",
+      rateMin: 0,
+      rateMax: 10,
+    }],
+    visibleIf: "{satisfaction-score} >= 4"
+  }, {
+    elements: [{
+      name: "how-can-we-improve",
+      title: "In your opinion, how could we improve our product?",
+      type: "comment"
+    }],
+    visibleIf: "{satisfaction-score} = 3"
+  }, {
+    elements: [{
+      name: "disappointing-experience",
+      title: "Please let us know why you had such a disappointing experience with our product",
+      type: "comment"
+    }],
+    visibleIf: "{satisfaction-score} =< 2"
+  }],
+  showQuestionNumbers: "off",
+  pageNextText: "Forward",
+  completeText: "Submit",
+  showPrevButton: false,
+  firstPageIsStarted: true,
+  startSurveyText: "Take the Survey",
+  completedHtml: "Thank you for your feedback!",
+  showPreviewBeforeComplete: "showAnsweredQuestions"
+};
 
 function SurveyPage() {
+  // useRef enables the Model object to persist between state changes
+  const survey = useRef(new Model(surveyJson)).current;
+  const [surveyResults, setSurveyResults] = useState("");
+  const [isSurveyCompleted, setIsSurveyCompleted] = useState(false);
+
+  const displayResults = useCallback((sender) => {
+    setSurveyResults(JSON.stringify(sender.data, null, 4));
+    setIsSurveyCompleted(true);
+  }, []);
+
+  survey.onComplete.add(displayResults);
+  survey.applyTheme(LayeredDark);
   return (
-    <div>SurveyPage</div>
-  )
+    <div id='survey-container'>
+      <Survey model={survey} id="surveyContainer" />
+      {isSurveyCompleted && (
+        <>
+          <p>Result JSON:</p>
+          <code style={{ whiteSpace: 'pre' }}>
+            {surveyResults}
+          </code>
+        </>
+        )
+      }
+    </div>
+  );
 }
 
-export default SurveyPage
+export default SurveyPage;
